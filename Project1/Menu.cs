6a0421d8ef8d4b583113c02bbcebc45288631061
jarmolowiczmagdalena5 <jarmolowiczmagdalena5@gmail.com>
2022-06-11 23:16:15 +0200

    Added calculator and menu

---
+++ Project1/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Menu
+    {
+        public void BasicMenu()
+        {
+            Console.WriteLine("Wybierz funkcję w menu:");
+            Console.WriteLine("1. Kalkulator");
+            Console.WriteLine("2. Liczba pierwsza, Silnia, Liczba doskonała, Potęgi");
+        }
+        public void Calculator()
+        {
+            Console.WriteLine("Wybierz funkcję kalkluatora:");
+            Console.WriteLine("1. Dodawanie");
+            Console.WriteLine("2. Odejmowanie");
+            Console.WriteLine("3. Mnożenie");
+            Console.WriteLine("4. Dzielenie");
+        }
+        public void ExtendedCalculator()
+        {
+            Console.WriteLine("Wybierz funkcję:");
+            Console.WriteLine("1. Liczba pierwsza");
+            Console.WriteLine("2. Silnia");
+            Console.WriteLine("3. Liczba doskonała");
+            Console.WriteLine("4. Potęgi");
+            Console.WriteLine("5. Średnia liczb");
+            Console.WriteLine("6. Największa i najmniejsza liczba ze zbioru");
+        }
+    }
+}
--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Project1;
+
+Menu basicMenu = new Menu();
+BasicCalculator BasicCalculator = new BasicCalculator();
+
+basicMenu.BasicMenu();
+int basicMenuChoice = Convert.ToInt32(Console.ReadLine());
+switch (basicMenuChoice)
+{
+    case 1:
+        basicMenu.Calculator();
+        break;
+    case 2:
+        basicMenu.ExtendedCalculator();
+        break;
+}
+int basicCalculatorChoice = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Podaj liczbe 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Podaj liczbe 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+switch (basicCalculatorChoice)
+{
+    case 1:
+        Console.WriteLine(BasicCalculator.Addiction(number1, number2));
+        break;
+    case 2:
+        Console.WriteLine(BasicCalculator.Subtraction(number1, number2));
+        break;
+    case 3:
+        Console.WriteLine(BasicCalculator.Multiplication(number1, number2));
+        break;
+    case 4:
+        Console.WriteLine(BasicCalculator.Division(number1, number2));
+        break;
+}
