e3664be5005476897e3aa361c1581b96f5564dfb
jarmolowiczmagdalena5 <jarmolowiczmagdalena5@gmail.com>
2022-06-12 11:19:10 +0200

    using extantion metod

--- Project1/BasicCalculator.cs
+++ Project1/BasicCalculator.cs
@@ -1,28 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Project1
+﻿namespace Project1
 {
     internal class BasicCalculator
     {
-        public int Addiction(int number1, int number2)
-        {
-            return number1 + number2;
-        }
-        public int Subtraction(int number1, int number2)
-        {
-            return number1 - number2;
-        }
-        public int Multiplication(int number1, int number2)
-        {
-            return number1 * number2;
-        }
-        public int Division(int number1, int number2)
-        {
-            return number1 / number2;
-        }
+        public int Addiction(int number1, int number2) => number1 + number2;
+        public int Subtraction(int number1, int number2) => number1 - number2;
+        public int Multiplication(int number1, int number2) => number1 * number2;
+        public int Division(int number1, int number2) => number1 / number2;
     }
 }
