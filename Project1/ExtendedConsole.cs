3f84903a8e7fb0b576e53173789a1e4e08fd1cb1
jarmolowiczmagdalena5 <jarmolowiczmagdalena5@gmail.com>
2022-07-14 17:25:51 +0200

    added colors

---
+++ Project1/ExtendedConsole.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class ExtendedConsole
+    {
+        public static void WriteApprove(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+        public static void WriteError(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Project1/TextFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class TextFile
+    {
+        public void ReadAllText()
+        {
+            /*wczytanie całego pliku*/
+        }
+        public void ReadTheLine()
+        {
+            /*tu trzeba dać tablicę*/
+        }
+        public void AddTextToFile()
+        {
+            /*dodanie tekstu do konkretnej linii*/
+        }
+        public void DeleteTheLine()
+        {
+            /*usuwanie konkretnej linii*/
+        }
+    }
+}
